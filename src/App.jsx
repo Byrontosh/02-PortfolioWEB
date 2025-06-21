f06943eaa7ebbde4d2552b6d3c2136df0a76056f
Byrontosh <by_tosh20@hotmail.com>
2025-06-21 07:19:41 -0500

    PortfolioWEB

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Header from "./components/header/Header";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Landing from "./pages/Landing";
+import Expertice from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
   return (
-      <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path="login" element={<Expertice />} />
+        <Route path="register" element={<Register />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,23 @@
 import { Main } from '../main/Main';
 import './Header.css';
+import { Link } from "react-router";
 
 const Header = () => {
     return (
 
         <header>
-            <div class="navbar__container">
+            <nav class="navbar__container">
                 <p class="navbar__logo">Byrontosh</p>
                 <ul class="navbar__menu">
-                    <li><a href="#navbar">Inicio</a></li>
-                    <li><a href="#bout">Sobre mí</a></li>
-                    <li><a href="#portfolio">Proyectos</a></li>
-                    <li><a href="#expert">Experticia</a></li>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#about">Sobre mí</a></li>
+                    <li><a href="#project">Proyectos</a></li>
+                    <li><a to="#expertice">Experticia</a></li>
                     <li><a href="#contact">Contacto</a></li>
+                    <li><Link to="/login">Ingresar</Link></li>
+                    <li><Link to="/register">Registrarse</Link></li>
                 </ul>
-            </div>
+            </nav>
             <Main />
         </header>
 
